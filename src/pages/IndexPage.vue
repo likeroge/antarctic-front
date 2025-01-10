dd082dff38077ce966d1ce3d90e606e26f6e9f96
likeroge <likeroge@gmail.com>
2025-01-10 22:55:30 +0400

    init

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,7 +1,12 @@
 <template>
   <q-page class="row items-center justify-evenly">
     <h1>Main page</h1>
+    <!-- <p>{{ process.env.VITE_APP_API_URL }}</p> -->
+    <p>{{ data }}</p>
   </q-page>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+const process = window.process
+const data = process.env.VITE_APP_API_URL
+</script>
